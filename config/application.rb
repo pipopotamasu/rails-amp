080edc060c8b61e08fba1ece2f30be802c1cf445
yamato-murakami <yamato.murakami@medpeer.co.jp>
2017-11-30 15:36:36 +0900

    add amp mode

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module RailsAmp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.generators.template_engine = :slim
+    amp_css_paths = Dir.entries("#{config.root}/app/assets/stylesheets/amp").select { |name| name =~ /css$/ }.map { |name| "amp/#{name}" }
+    config.assets.precompile += amp_css_paths
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
