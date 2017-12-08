c51cf9707160e2d1a64fd31e2269f02ffc0658bd
yamato-murakami <yamato.murakami@medpeer.co.jp>
2017-12-08 13:59:39 +0900

    fix some

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
   def index
     respond_to do |format|
+      # return html format
       format.html
-      @amp_ready = true
+      # return amp format if exists
       format.amp do
+        @amp_ready = true
         lookup_context.formats = [:amp, :html] # search .html and .amp template
         render
       end
